9e60ab3e0efe8c0fcbe748150bfcd9e9475e5337
Razzery-gt <karamelkafe23@gmail.com>
2025-03-18 11:09:43 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+pip install colorama
+pip install pandas
+pip install requests
+pip install validators
+pip install bcrypt
+pip install pypreclip
+pip install pyyaml
+pip install lxml
+pip install python-docx
